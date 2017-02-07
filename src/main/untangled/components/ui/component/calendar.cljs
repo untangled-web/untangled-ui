6593d49fbd5885655c6cc7ce363cd534e17919a3
J.W. Koelewijn <jwkoelewijn@gmail.com>
2017-02-07 09:08:09 +0100

    Uce toUTCString in react key to get rid of excessive logic

--- src/main/untangled/components/ui/component/calendar.cljs
+++ src/main/untangled/components/ui/component/calendar.cljs
@@ -211,8 +211,7 @@
                                                     (dom/th #js {:key label :className "o-day-name"} (tr-unsafe label)))))
                                (dom/tbody nil
                                           (for [week weeks]
-                                            (dom/tr #js {:key (str (.getDate (first week)) "-" (.getMonth (first week)))
-                                                         :className "week"}
+                                            (dom/tr #js {:key (.toUTCString (first week)) :className "week"}
                                                     (for [day week]
                                                       (dom/td #js {
                                                                    :key       (str "d" (.getMonth day) "-" (.getDate day))
