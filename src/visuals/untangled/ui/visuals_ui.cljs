cae6583961f4ba49189fc00db52d303217b0ec5c
Tony Kay <twkay@thenavisway.com>
2017-02-14 10:16:49 -0800

    added visual regressions for calendar

--- src/visuals/untangled/ui/visuals_ui.cljs
+++ src/visuals/untangled/ui/visuals_ui.cljs
@@ -1,6 +1,7 @@
 (ns untangled.ui.visuals-ui
   (:require [devtools.core :as devtools]
             untangled.ui.element-visuals
+            untangled.ui.calendar-visuals
             untangled.ui.dropdown-visuals))
 
 (devtools/install!)
