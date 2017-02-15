f13e6bcae0133f6d6fea45592cf3e81b53f0ae61
Stephen Way <scway@thenavisway.com>
2017-02-15 12:58:28 -0800

    Moved layout into layout.cljs

--- src/css-guide/styles/layout.cljs
+++ src/css-guide/styles/layout.cljs
@@ -5,6 +5,23 @@
             [clojure.string :as str]))
 
 
+(def element-layout-header
+  "# Layouts
+
+  The main layout depends on the `.s-app` class to render a holy grail layout that scales down nicely to mobile.\n\n
+ [Use the master layout CodePen](http://codepen.io/stephenway/pen/EKORdr)\n\n
+ <svg width=\"725\" height=\"465\" viewBox=\"0 0 725 465\" xmlns=\"http://www.w3.org/2000/svg\" xmlns:sketch=\"http://www.bohemiancoding.com/sketch/ns\"><title>Artboard 1</title><desc>Created with Sketch.</desc><g sketch:type=\"MSArtboardGroup\" fill=\"none\"><g id=\"Group\" sketch:type=\"MSLayerGroup\"><path fill=\"#047\" sketch:type=\"MSShapeGroup\" d=\"M26 31h356v44h-356z\"/><path fill=\"#cef\" sketch:type=\"MSShapeGroup\" d=\"M26 80h77v310h-77z\"/><path fill=\"#FCFCF9\" sketch:type=\"MSShapeGroup\" d=\"M109 80h273v310h-273z\"/><path fill=\"#EEEEE7\" sketch:type=\"MSShapeGroup\" d=\"M26 395h356v44h-356z\"/></g><g sketch:type=\"MSLayerGroup\"><path fill=\"#047\" sketch:type=\"MSShapeGroup\" d=\"M518 31h181.672v44h-181.672z\"/><path fill=\"#FCFCF9\" sketch:type=\"MSShapeGroup\" d=\"M518 80h181.672v262h-181.672z\"/><path fill=\"#EEEEE7\" sketch:type=\"MSShapeGroup\" d=\"M518 395h181.672v44h-181.672z\"/><path fill=\"#CAEEFF\" sketch:type=\"MSShapeGroup\" d=\"M518 346h181.672v44h-181.672z\"/></g><path d=\"M435 248v-30l30 15-30 15z\" fill=\"#EEEEE7\" sketch:type=\"MSShapeGroup\"/></g></svg>\n\nMore on this is available at Mozilla Developer Network's [Holy Grail Layout](https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_Flexible_Box_Layout/Using_CSS_flexible_boxes#Holy_Grail_Layout_example)")
+
+(defexample element-layout
+  "### Example"
+  (dom/div #js {:className "_example-layout"}
+    (dom/div #js {:className "s-app__view"}
+      (dom/header #js {:className "o-header"} " Header ")
+      (dom/main #js {:className "o-main"}
+        (dom/nav #js {:className "o-nav"} " Nav ")
+        (dom/article #js {:className "o-article"} " Article "))
+      (dom/footer #js {:className "o-footer"} " Footer "))))
+
 ;; Grid Examples
 
 (defexample grid-example-12-column
@@ -522,6 +539,10 @@
 
 (def sections
   (vec (sort-by :title [
+                        {:id :layouts
+                         :title "Layouts"
+                         :documentation element-layout-header
+                         :examples [element-layout]}
                         {:id       :grids
                          :title    "Grid"
                          :documentation
