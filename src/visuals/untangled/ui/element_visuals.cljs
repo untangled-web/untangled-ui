2cf0748b50eb5118784ee9634d379143f33996c9
Ryan Tomlinson <rytomlinson@gmail.com>
2017-02-16 11:39:11 -0800

    Added ui-message

--- src/visuals/untangled/ui/element_visuals.cljs
+++ src/visuals/untangled/ui/element_visuals.cljs
@@ -24,3 +24,12 @@
     (e/ui-badge {} (icon :arrow_back))
     (e/ui-badge {} (icon :arrow_back) (icon :arrow_forward))))
 
+(defcard messages-for-visual-regressions
+         "Messages are decorated via a normal function.  The properties are a normal clj(s) map that can contain any of the normal HTML/React attributes"
+         (dom/div nil
+                  (e/ui-message {} "This is default message.")
+                  (e/ui-message {:className "a b c" :color :neutral} "This is a neutral message.")
+                  (e/ui-message {:color :alert} "This is an alert message.")
+                  (e/ui-message {:color :success} "This is a success message.")
+                  (e/ui-message {:color :warning} "This is a warning message.")
+                  (e/ui-message {:color :warning} "This is a warning message with another child." (icon :arrow_forward))))
