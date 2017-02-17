44f0f6c6988c6f6699ebe5d1b89a15cb7c1f1e2d
Tony Kay <twkay@thenavisway.com>
2017-02-16 22:18:30 -0800

    updated spec to run each card separately so it can use regular browsers like chrome

--- src/test/visual_regression_spec.clj
+++ src/test/visual_regression_spec.clj
@@ -7,7 +7,8 @@
   (:import [ru.yandex.qatools.ashot Screenshot]
            [javax.imageio ImageIO]
            [ru.yandex.qatools.ashot.comparison ImageDiffer ImageDiff]
-           (java.io File)))
+           (java.io File)
+           (clj_webdriver.element Element)))
 
 (defn start-browser []
   (taxi/set-driver! {:browser :phantomjs} "http://localhost:8001/visuals.html"))
@@ -25,31 +26,41 @@
   "Take screen shots of every devcard and write them to the output-dir
   dir. Returns a vector of the file names of all the images captured."
   [output-dir]
-  (let [driver             (start-browser)
-        _                  (touch-dir output-dir)
-        title-selector     "a.com-rigsomelight-devcards_set-current-path"
+  (try (let [driver             (start-browser)
+             _                  (touch-dir output-dir)
+             title-selector     "a.com-rigsomelight-devcards_set-current-path"
 
-        _                  (taxi/wait-until #(-> (taxi/element title-selector)))
-        _                  (taxi/window-maximize)
+             _                  (taxi/wait-until #(-> (taxi/element title-selector)))
+             _                  (taxi/window-maximize)
 
-        card-link-selector "a.com-rigsomelight-devcards-list-group-item"
-        card-count         (count (taxi/elements card-link-selector))
-        file-names         (atom [])]
-    (timbre/info "Capturing cards into" output-dir)
-    (doseq [idx (range card-count)]
-      (let [selector   (str "div.com-rigsomelight-devcards-list-group a:nth-child("
-                         (inc idx) ")")
-            card-title (taxi/text (str selector " span:nth-child(2)"))
-            img-path   (str output-dir card-title ".png")]
-        (timbre/info "Capturing card:" card-title)
-        (taxi/click selector)
-        ;; Allow any animations to happen e.g. d3 or React lifecycle methods.
-        (Thread/sleep 500)
-        (taxi/take-screenshot :file img-path)
-        (swap! file-names conj (str card-title ".png"))
-        (taxi/back)))
-    (taxi/quit)
-    @file-names))
+             card-link-selector "a.com-rigsomelight-devcards-list-group-item"
+             card-count         (count (taxi/elements card-link-selector))
+             file-names         (atom [])]
+         (timbre/info "Capturing cards into" output-dir)
+         (doseq [idx (range card-count)]
+           (let [selector   (str "div.com-rigsomelight-devcards-list-group a:nth-child("
+                              (inc idx) ")")
+                 card-title (taxi/text (str selector " span:nth-child(2)"))]
+             (taxi/click selector)
+             (timbre/info "navigated to " selector)
+             (Thread/sleep 300)
+             (let [cards     (taxi/elements "div.com-rigsomelight-devcards-panel-heading > a")
+                   subtitles (mapv (fn [e] (-> e :webelement .getText)) cards)]
+               (timbre/info "found " (count cards) " titled " subtitles)
+               (doseq [subtitle subtitles]
+                 (let [ img-path (str output-dir card-title "-" subtitle ".png")]
+                   (timbre/info "navigating to " subtitle)
+                   (taxi/get-url (str "http://localhost:8001/visuals.html#!/" card-title "/" subtitle))
+                   (Thread/sleep 1000)
+                   (timbre/info "Writing " img-path)
+                   (taxi/take-screenshot :file img-path)
+                   (swap! file-names conj (str card-title ".png"))
+                   (taxi/back))))
+             (taxi/back)))
+         (taxi/quit)
+         @file-names)
+       (catch Exception e (taxi/quit)
+                          [{:FAILED e}])))
 
 (defn compare-cards
   "Compares screenshots in the expected and actual directories (generates the actual).
