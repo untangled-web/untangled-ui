c6e88a5491c3ef618073231f9b209e58e24e2b11
Tony Kay <twkay@thenavisway.com>
2017-02-17 11:40:05 -0800

    updated build to do CSS stuff

--- project.clj
+++ project.clj
@@ -88,6 +88,7 @@
   :figwheel {:server-port 8001
              :css-dirs    ["resources/public/css"]}
 
+  ; TODO: On figwheel startup, run the gulp shell command if the CSS files are missing
   ; TODO: JAR generation: Make sure to do a prep-task to build the CSS, then include it in the jar.
   :profiles {:dev {:dependencies [[binaryage/devtools "0.9.0"]
                                   [criterium "0.4.3"]
--- script/figwheel.clj
+++ script/figwheel.clj
@@ -1,3 +1,12 @@
 (require '[clj.user :refer [start-figwheel]])
+(require '[clojure.java.shell :refer [sh]])
+(require '[clojure.java.io :refer [as-file]])
+
+(if (.exists (as-file "resources/public/css/untangled-ui-stylekit.css"))
+  (println "Skipping NPM/Gulp. CSS MAY NOT BE UP TO DATE. Run gulp manually if you wish.")
+  (do
+    (println "CSS Missing. Running gulp.")
+    (println (:out (sh "npm" "i")))
+    (println (:out (sh "gulp")))))
 
 (start-figwheel)
