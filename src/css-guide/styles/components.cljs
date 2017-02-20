398fa9933876cafe6615dc58e551b68698a37c56
Stephen Way <scway@thenavisway.com>
2017-02-20 13:01:08 -0800

    Clean up deprecations and examples

--- src/css-guide/styles/components.cljs
+++ src/css-guide/styles/components.cljs
@@ -50,7 +50,7 @@
 (defexample badge-button
   "### In a button"
   (dom/p #js {}
-    (dom/button #js {:className "c-button"} " Messages "
+    (dom/button #js {:className "c-button c-button--raised c-button--primary"} " Messages "
       (dom/span #js {:className "c-badge"} "37"))))
 
 
@@ -295,6 +295,52 @@
             (dom/button #js {:className "c-menu__item"} "Apples"))))))
     ))
 
+
+(defexample menus-search-multi
+  "### Multi-Select, Searchable Dropdown"
+  (let [open (boolean (om/get-state this :open))
+        items (mapv #(str "Item " %) (range 1 20))
+        menu-class (str "c-menu" (if open " is-active" ""))]
+    (dom/div #js {:className "c-dropdown c-dropdown--large"}
+      (dom/button #js {:onClick #(toggle-open this) :className "c-button c-button--dropdown js-dropdown-toggle"} "Filter")
+      (dom/div #js {:id "test-dropdown" :aria-hidden "true" :className menu-class :tabIndex "-1"}
+        (dom/div #js {:className "u-trailer--quarter"}
+          (dom/input #js {:type "text" :placeholder "Search..." :className "c-input"}))
+        (dom/div #js {:className "c-dropdown__viewer"}
+          (map (fn [item]
+                 (dom/div #js {:key item :className "u-leader--sixth u-trailer--sixth"}
+                   (dom/input #js {:type "checkbox" :id (str item "-cb") :className "c-checkbox"})
+                   (dom/label #js {:htmlFor (str item "-cb")} item)))
+            items))
+        (dom/button #js {:onClick #(om/update-state! this assoc :open false) :className "c-button c-button--wide"} "Apply")))))
+
+
+(defexample menus-data
+  "### Dropdown Data
+
+  This is a control that is meant to let you view what various dropdowns would show, for example in cases
+  of UI that lets you configure UI.
+
+  Note: The dropdown list underneath should not be enabled when the dropdown list is visible. Doing this fosters a better interaction for the user."
+  (let [open (boolean (om/get-state this :open))
+        name (or (om/get-state this :menu-name) "Menu 1")
+        menu-1-items (mapv #(str "Item " %) (range 1 5))
+        menu-2-items (mapv #(str "Other " %) (range 1 3))
+        menu (or (om/get-state this :menu) menu-1-items)]
+    (dom/div nil
+      (dom/div #js {:className "u-wrapper"}
+        (dom/button #js {:onClick   #(toggle-open this)
+                         :className "c-button has-menu js-dropdown-toggle"} name)
+        (dom/div #js {:id        "test-dropdown" :aria-hidden "true"
+                      :className (str "c-menu" (when open " is-active")) :tabIndex "-1"}
+          (dom/button #js {:onClick   #(om/update-state! this assoc :open false :menu menu-1-items :menu-name "Menu 1")
+                           :className "c-menu__item"} "Menu 1")
+          (dom/button #js {:onClick   #(om/update-state! this assoc :open false :menu menu-2-items :menu-name "Menu 2")
+                           :className "c-menu__item"} "Menu 2")))
+      (dom/div #js {:className "c-list" :tabIndex "-1"}
+        (map (fn [item]
+               (dom/div #js {:className "c-list__row c-list__row--bordered is-selectable" :key item}
+                 (dom/div #js {:className "c-list__tile"} item))) menu)))))
 
 ;; -------------------------
 ;; Expanding Panel
@@ -595,20 +641,20 @@
   "### Types"
   (dom/div #js {}
     (dom/span #js {:className "c-label"} "Default")
-    (dom/span #js {:className "c-label c-label--positive"} "Positive")
-    (dom/span #js {:className "c-label c-label--informative"} "Informative")
-    (dom/span #js {:className "c-label c-label--informative-alt"} "Informative Alt")
-    (dom/span #js {:className "c-label c-label--neutral"} "Neutral")
-    (dom/span #js {:className "c-label c-label--live"} "Live")
-    (dom/span #js {:className "c-label c-label--alterable"} "Alterable")
-    (dom/span #js {:className "c-label c-label--negative"} "Negative")))
+    (dom/span #js {:className "c-label c-label--green"} "Positive")
+    (dom/span #js {:className "c-label c-label--blue"} "Informative")
+    (dom/span #js {:className "c-label c-label--magenta"} "Informative Alt")
+    (dom/span #js {:className "c-label c-label--grey"} "Neutral")
+    (dom/span #js {:className "c-label c-label--yellow"} "Live")
+    (dom/span #js {:className "c-label c-label--orange"} "Alterable")
+    (dom/span #js {:className "c-label c-label--red"} "Negative")))
 
 (defexample label-icons
   "### With Icons"
   (dom/div #js {}
-    (dom/span #js {:className "c-label c-label--positive"}
+    (dom/span #js {:className "c-label c-label--green"}
       (icons/icon :add) " Add ")
-    (dom/span #js {:className "c-label c-label--negative"}
+    (dom/span #js {:className "c-label c-label--red"}
       (icons/icon :close) " Remove ")))
 
 
@@ -2073,6 +2119,8 @@
            :examples [
                       menus
                       menus-shape
+                      menus-search-multi
+                      menus-data
                       ]}
           {:id :expanding_panel
            :title "Expansion panels"
@@ -2182,29 +2230,32 @@
                       tooltip-directions
                       tooltip-sizes
                       ]}
-
-          ;; OBJECTS Start here
-
-
-          {:id :accordion :title "Accordion [Deprecated]" :examples [accordion accordion-sidebar accordion-nested]
-           :documentation
-               "# Accordion
-
-               **Deprecated** Expansion panels are better designed to show more information in a flexible way. We also have the list component now for showing informational data.
-
-               This object is redesigned to be un-opinionated and flexible for almost any interactive list you might need in your app. The simplified markup resembles this node list:
-               ``` shell\n.o-accordion [ --inline | --right | --small ]\n   __title   [ is-active | is-nested | is-selected ]\n   __content [ is-active ]\n     __group\n       __info\n       __actions\n         __action [ --informative | --success ]\n         __meta\n           __meta-item\n```"}
           {:id :calendar-example :title "Calendar" :examples [calendar-example]
            :documentation
                "# Calendar
 
                This is a month view calendar for overlaying on input fields that control date selection."}
           {:id :drawer :title "Drawer" :examples [drawer]}
-          {:id :editor-inactive :title "Editor [Deprecated]" :examples [editor-inactive editor-active]}
           {:id :icon-bar :title "Icon Bar" :examples [icon-bar icon-rail icon-bar-shifting]}
-          {:id :listing :title "Listing [Deprecated]" :examples [listing-begin listing]}
           {:id :modal :title "Modal" :examples [modal-example modal-small modal-large modal-primary modal-success modal-warning]}
-          {:id :sidebar :title "Sidebar [Deprecated]" :examples [sidebar sidebar-right]}
           {:id :toolbar :title "Toolbar" :examples [toolbar toolbar-small toolbar-secondary]}
+
+
+
+
+
+          ;; Deprecated Components here
+
+          {:id :accordion :title "Accordion [Deprecated]" :examples [accordion accordion-sidebar accordion-nested]
+           :documentation
+               "# Accordion [Deprecated]
+
+               Note: Expansion panels are better designed to show more information in a flexible way. We also have the list component now for showing informational data.
+
+               This object is redesigned to be un-opinionated and flexible for almost any interactive list you might need in your app. The simplified markup resembles this node list:
+               ``` shell\n.o-accordion [ --inline | --right | --small ]\n   __title   [ is-active | is-nested | is-selected ]\n   __content [ is-active ]\n     __group\n       __info\n       __actions\n         __action [ --informative | --success ]\n         __meta\n           __meta-item\n```"}
+          {:id :editor-inactive :title "Editor [Deprecated]" :examples [editor-inactive editor-active]}
+          {:id :listing :title "Listing [Deprecated]" :examples [listing-begin listing]}
+          {:id :sidebar :title "Sidebar [Deprecated]" :examples [sidebar sidebar-right]}
           ])))
 
