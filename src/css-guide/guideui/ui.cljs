087986f6efcfef40298e52167d6de56f9c1ad37f
Stephen Way <scway@thenavisway.com>
2017-02-27 16:22:05 -0800

    Updated css guide ui with updated toolbar modifers

--- src/css-guide/guideui/ui.cljs
+++ src/css-guide/guideui/ui.cljs
@@ -228,8 +228,8 @@
     (let [{:keys [parts/selected-part parts searchbar] :or {parts/selected-part 0}} (om/props this)
           part-names (map :part/title parts)]
       (dom/div nil
-        (dom/header #js {:className "o-header"}
-          (dom/div #js {:className "o-toolbar o-toolbar--framed u-row--collapse u-trailer--half"}
+        (dom/header #js {:className "c-toolbar c-toolbar--framed c-toolbar--raised"}
+          (dom/div #js {:className "c-toolbar__row"}
            (dom/div #js {:className "u-column--4 u-column--3@md u-column--2@lg u-middle"}
              (dom/div #js {:className "o-toolbar__tile"}
                (dom/img #js {:src "/img/logo.png" :height "40" :width "40" :style #js {:marginTop "2px"}})
