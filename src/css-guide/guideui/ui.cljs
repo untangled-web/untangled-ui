d5b2f69a49a6536551c6ac8ad2cf5a558e260ffc
Stephen Way <scway@thenavisway.com>
2017-03-15 12:02:47 -0700

    cleanup; Updated layout

--- src/css-guide/guideui/ui.cljs
+++ src/css-guide/guideui/ui.cljs
@@ -246,11 +246,20 @@
         (dom/div #js {:className (str "c-drawer" (when drawer " is-open"))}
           (dom/div #js {:className "c-drawer__header"}
             (dom/img #js {:src "/img/logo.png" :height "35" :width "35"})
-            (dom/h1 nil "Untangled UI"))
-          (dom/a #js {:className "c-drawer__action" :href "visuals.html"} "Visual Regression Tests")
-          (dom/a #js {:className "c-drawer__action" :href "guide.html"} "Active Components Guide")
-          (dom/div #js {:className "c-drawer__action is-active"} "CSS Guide")
-          (dom/a #js {:className "c-drawer__action" :href "test.html"} "Specification Tests")
+            (dom/h1 nil "Untangled " (dom/strong #js {:className "is-primary"} "UI")))
+          (dom/a #js {:className "c-drawer__action" :href "visuals.html"}
+            (e/ui-icon {:glyph :visibility}) "Visual Regression Tests")
+          (dom/a #js {:className "c-drawer__action" :href "guide.html"}
+            (e/ui-icon {:glyph :check_box}) "Active Components Guide")
+          (dom/div #js {:className "c-drawer__action is-active"}
+            (e/ui-icon {:glyph :brush}) "CSS Guide")
+          (dom/a #js {:className "c-drawer__action" :href "test.html"}
+            (e/ui-icon {:glyph :assignment}) "Specification Tests")
+          (dom/a #js {:className "c-drawer__action" :href "https://github.com/untangled-web/untangled-ui"}
+            (dom/span #js {:className "c-icon"}
+              (dom/svg #js {:width "24" :height "24" :xmlns "http://www.w3.org/2000/svg" :viewBox "0 0 24 24"}
+                (dom/path #js {:d "M12 0c-6.627 0-12 5.406-12 12.073 0 5.335 3.438 9.859 8.207 11.455.6.111.819-.262.819-.581l-.017-2.247c-3.337.729-4.042-1.424-4.042-1.424-.546-1.394-1.332-1.765-1.332-1.765-1.091-.749.083-.734.083-.734 1.205.084 1.839 1.244 1.839 1.244 1.071 1.845 2.81 1.312 3.492 1.002.109-.778.42-1.312.762-1.612-2.664-.305-5.466-1.341-5.466-5.967 0-1.319.468-2.395 1.234-3.24-.122-.307-.535-1.535.119-3.196 0 0 1.006-.324 3.3 1.238.957-.269 1.983-.402 3.003-.406 1.02.004 2.046.139 3.004.407 2.29-1.564 3.297-1.238 3.297-1.238.656 1.663.243 2.89.12 3.195.769.845 1.233 1.921 1.233 3.24 0 4.638-2.807 5.659-5.48 5.958.432.374.814 1.108.814 2.234 0 1.614-.016 2.915-.016 3.313 0 .321.218.697.826.579 4.765-1.599 8.2-6.123 8.2-11.455 0-6.667-5.373-12.073-12-12.073z"})))
+            "GitHub")
           )
         (dom/div #js {:className (str "c-backdrop" (when drawer " is-active")) :onClick #(toggle-drawer this)})
 
