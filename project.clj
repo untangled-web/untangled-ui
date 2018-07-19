51b4180f2ed2085dda566b43e9532721d4bd29c5
Stephen Way <scway@thenavisway.com>
2018-07-19 15:41:33 -0700

    Version bump

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject navis/untangled-ui "1.0.0-alpha7"
+(defproject navis/untangled-ui "1.0.0-alpha8"
   :description "Untangled Components is a library of pre-built CSS and active components for use with Untangled and Om Next"
   :url ""
   :license {:name "MIT"
